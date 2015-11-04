99fa29369a36cd9efa4db54ac78662bfe51836f9
sskhaustov <xaystov95@gmail.com>
2015-11-04 19:59:15 +0300

    master

---
+++ CKOT/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace CKOT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.screen = new System.Windows.Forms.PictureBox();
+            this.draw = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.t_step = new System.Windows.Forms.Label();
+            this.res = new System.Windows.Forms.Button();
+            this.draw_back = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.screen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // screen
+            // 
+            this.screen.BackColor = System.Drawing.SystemColors.Control;
+            this.screen.Location = new System.Drawing.Point(2, 3);
+            this.screen.Name = "screen";
+            this.screen.Size = new System.Drawing.Size(510, 510);
+            this.screen.TabIndex = 0;
+            this.screen.TabStop = false;
+            // 
+            // draw
+            // 
+            this.draw.Location = new System.Drawing.Point(409, 514);
+            this.draw.Name = "draw";
+            this.draw.Size = new System.Drawing.Size(95, 23);
+            this.draw.TabIndex = 1;
+            this.draw.Text = "NEXT";
+            this.draw.UseVisualStyleBackColor = true;
+            this.draw.Click += new System.EventHandler(this.draw_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 519);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ШАГ:";
+            // 
+            // t_step
+            // 
+            this.t_step.AutoSize = true;
+            this.t_step.Location = new System.Drawing.Point(54, 519);
+            this.t_step.Name = "t_step";
+            this.t_step.Size = new System.Drawing.Size(13, 13);
+            this.t_step.TabIndex = 3;
+            this.t_step.Text = "0";
+            // 
+            // res
+            // 
+            this.res.Location = new System.Drawing.Point(224, 514);
+            this.res.Name = "res";
+            this.res.Size = new System.Drawing.Size(86, 23);
+            this.res.TabIndex = 4;
+            this.res.Text = "RESET";
+            this.res.UseVisualStyleBackColor = true;
+            this.res.Click += new System.EventHandler(this.res_Click);
+            // 
+            // draw_back
+            // 
+            this.draw_back.Location = new System.Drawing.Point(316, 514);
+            this.draw_back.Name = "draw_back";
+            this.draw_back.Size = new System.Drawing.Size(87, 23);
+            this.draw_back.TabIndex = 5;
+            this.draw_back.Text = "BACK";
+            this.draw_back.UseVisualStyleBackColor = true;
+            this.draw_back.Click += new System.EventHandler(this.draw_back_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 540);
+            this.Controls.Add(this.draw_back);
+            this.Controls.Add(this.res);
+            this.Controls.Add(this.t_step);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.draw);
+            this.Controls.Add(this.screen);
+            this.Name = "Form1";
+            this.Text = "treugolnik_serpinskogo";
+            ((System.ComponentModel.ISupportInitialize)(this.screen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox screen;
+        private System.Windows.Forms.Button draw;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label t_step;
+        private System.Windows.Forms.Button res;
+        private System.Windows.Forms.Button draw_back;
+    }
+}
+
---
+++ CKOT/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CKOT
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp; //изображение
+        Triangle T; //шаблон - рисование треугольника
+        Point A, B, C; //координаты вершин
+        int step; //текущий шаг фрактала
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(screen.Width, screen.Height); //создаем холст
+            //создаем исходный треугольник
+            A = new Point(0, 0); //
+            B = new Point(500, 0); //
+            C = new Point(500, 500); //<---- задаем координаты вершин
+            T = new Triangle(A, B, C);
+            screen.Image = T.draw_triangle(bmp); //<----- отрисовываем треугольник
+            step = 0; //<-------- исходный треугольник соответствует нулевому шагу
+        }
+        private void draw_Click(object sender, EventArgs e)
+        {
+            if (step < 10)
+            {
+                step++; //следующий шаг
+                t_step.Text = Convert.ToString(step);
+                Draw(T, step);
+            }
+        }
+        private void Draw(Triangle _T, int _n) //построение фрактала (на входе - треугольник и число оставшихся итерации)
+        {
+            //СУТЬ - разбиваем больший треугольник на 3 подобных и меньших по размеру, далее рекурсивно выполняем
+            //это же самое разбиение уже для дочерних треугольников, и делаем так нужное количество раз
+            if (_n >= 0) //всего n шагов, включая нулевой
+            {
+                screen.Image = _T.draw_triangle(bmp); //<--- это рисуем родительский треугольник
+                //координаты новых вершин - середин сторон треугольника
+                // Xan = (Xcn-1 - Xan-1) / 2 + Xan-1
+                // Yan = (Ycn-1 - Yan-1) / 2 + Yan-1
+                //т.е сумма половины разности предыдущих координат конца и начала стороны треугольника и координат начала
+                Point _A = new Point((_T.C.X - _T.A.X) / 2 + _T.A.X, (_T.C.Y - _T.A.Y) / 2 + _T.A.Y);
+                Point _B = new Point((_T.B.X - _T.A.X) / 2 + _T.A.X, _T.B.Y);
+                Point _C = new Point(_T.C.X, (_T.C.Y - _T.B.Y) / 2 + _T.B.Y);
+                //дочерние треугольники
+                Triangle _f = new Triangle(_T.A, _B, _A);
+                Triangle _s = new Triangle(_B, _T.B, _C);
+                Triangle _t = new Triangle(_A, _C, _T.C);
+                //рекурсивно вызываем функции для дочерних треугольников
+                //параметр итерации НЕ МЕНЯЕТСЯ, просто в каждую функцию передается входной, уменьшеный на единицу
+                //таким образом, все функции вызовятся одинаковое количество раз
+                Draw(_f, _n - 1);
+                Draw(_s, _n - 1);
+                Draw(_t, _n - 1);
+            }
+        }
+        private void res_Click(object sender, EventArgs e) //<--- сброс на нулевой шаг
+        {
+            bmp = new Bitmap(screen.Width, screen.Height); //старый холст выбрасываем и создаем новый
+            screen.Image = T.draw_triangle(bmp); //рисуем на нем исходный треугольник
+            step = 0; //сбрасываем шаг
+            t_step.Text = Convert.ToString(step);
+        }
+
+        private void draw_back_Click(object sender, EventArgs e) //деструкция, рисование в обратную сторону
+        {
+            if (step > 0)
+            {
+                bmp = new Bitmap(screen.Width, screen.Height); //перед рисованием очищаем холст
+                step--; //предыдущий шаг
+                t_step.Text = Convert.ToString(step);
+                Draw(T, step);
+            }
+        }
+    }
+}
